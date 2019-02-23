e2f309ee40a9e21b3e9770a12f34db67c97bab35
barbosaf <fabio.barbosa@blip.pt>
2019-02-23 13:34:45 +0000

    Add photo in properties projection

--- src/api/property/consts.ts
+++ src/api/property/consts.ts
@@ -1,7 +1,7 @@
 const logPrefix = '[api:property]:';
 const path = '/property';
 const projection = {
-  providerId: 1, createAt: 1, price: 1,
+  providerId: 1, createAt: 1, price: 1, photos: 1,
   provider: 1, status: 1, title: 1, type: 1,
   url: 1, topology: 1
 };
