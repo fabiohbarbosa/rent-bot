e24dc0cd9c69ba7f507265a52388fdc88e4f9209
barbosaf <fabio.barbosa@blip.pt>
2019-02-23 13:40:20 +0000

    Add ps in properties projection

--- src/api/property/consts.ts
+++ src/api/property/consts.ts
@@ -3,7 +3,7 @@ const path = '/property';
 const projection = {
   providerId: 1, createAt: 1, price: 1, photos: 1,
   provider: 1, status: 1, title: 1, type: 1,
-  url: 1, topology: 1
+  url: 1, topology: 1, ps: 1
 };
 const statusAvailableToPatch = [ 'OUT_OF_FILTER', 'MATCHED' ];
 
