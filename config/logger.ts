ee10daebd4156b23a318f95b2a9b614f008aad44
barbosaf <fabio.barbosa@blip.pt>
2019-03-30 23:28:46 +0000

    Add library to fix winston log level on stackdriver

--- config/logger.ts
+++ config/logger.ts
@@ -1,5 +1,6 @@
 import winston from 'winston';
 import props from './props';
+import { LoggingWinston } from '@google-cloud/logging-winston';
 
 export default winston.createLogger({
   level: props.logLevel,
@@ -12,5 +13,8 @@ export default winston.createLogger({
         .colorize(msg.level, `${msg.timestamp} ${msg.level}: ${msg.message}`)
     )
   ),
-  transports: new winston.transports.Console()
+  transports: [
+    new winston.transports.Console(),
+    LoggingWinston
+  ]
 });
